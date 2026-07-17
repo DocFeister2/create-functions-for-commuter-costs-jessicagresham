a791231607eabacc64d997fcf3543f892cfa5e2b
jessicagresham <jessica_gresham1@my.vcccd.edu>
2026-07-17 04:06:04 +0000

    fixed math error

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ double toCommutes(double gasDollars) {
   gasGallons = gasDollars / DOLLARS_PER_GALLON;
   gasMiles = gasGallons * MILES_PER_GALLON;
 
-  numCommutes = gasMiles; // TODO: That math is not right! Something is missing here...
+  // Fixed the math errror
+  numCommutes = gasMiles / MILES_PER_GALLON; // TODO: That math is not right! Something is missing here...
 
   return numCommutes;
 }
@@ -30,6 +31,8 @@ double toDollars(double commutes) {
   return totalCost;
 }
 */
+
+// Edit
 
 int main() {
 
